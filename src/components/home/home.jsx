b16acc34c4a2e5260dc5149ebc696bd9144d4868
zoltanmeszaros977 <zoltanmeszaros977@gmail.com>
2023-02-06 09:06:39 +0100

    Refactoring home page. Using CssTransition

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,189 +1,153 @@
-import "./home.css";
+import 'animate.css/animate.min.css';
+import styles from "./home.module.scss";
+import classnames from 'classnames';
+import env from 'react-dotenv';
 import React, { useState, useEffect, useRef } from "react";
+import { CSSTransition } from 'react-transition-group';
 
 export default function Home() {
   const [date, setDate] = useState(new Date().toISOString().slice(0, 10));
-  const [content, setContent] = useState(null);
+
+  const [currentContent, setCurrentContent] = useState(null);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [descriptionClass, setDescriptionClass] = useState("description");
-  const [imageWithDescriptionClass, setImageWithDescriptionClass] = useState(
-    "imageWithDescription"
-  );
+  const [showDescription, setShowDescription] = useState(false);
 
-  const componentRef = useRef();
+  const [showContent, setShowContent] = useState(true);
+  const [shiftLeft, setShiftLeft] = useState(true);
+  const [nextContent, setNextContent] = useState(null);
+  const [isContentOut, setIsContentOut] = useState(false);
+
   const contentRef = useRef();
+  
+  const callAPI = async () => {
+    setShowContent(false);
+    setNextContent(null);
 
-  const animate = (out) => {
-    if (!contentRef.current || !componentRef.current) return;
-
-    const node = contentRef.current;
-    const nodeInfo = node.getBoundingClientRect();
-    const clonedNode = node.cloneNode(true);
-    const parent = componentRef.current;
-
-    if (out) setImageWithDescriptionClass("imageWithDescription hide");
-
-    parent.insertBefore(clonedNode, parent.firstElementChild);
-
-    clonedNode.style.position = "fixed";
-    clonedNode.style.zIndex = 2;
-
-    if (out) clonedNode.style.left = Math.floor(nodeInfo.left) + "px";
-    else {
-      clonedNode.style.visibility = "visible";
-      clonedNode.style.left = "-70%";
-    }
-
-    clonedNode.style.top = Math.floor(nodeInfo.top) + "px";
-    clonedNode.style.transition = "left 2s";
-
-    setTimeout(() => {
-      if (out) clonedNode.style.left = "100%";
-      else clonedNode.style.left = Math.floor(nodeInfo.left) + "px";
-    }, 100);
-
-    setTimeout(() => {
-      clonedNode.remove();
-
-      if (!out) setImageWithDescriptionClass("imageWithDescription");
-    }, 2100);
-  };
-
-  const callingTheAPI = async () => {
-    const response = await fetch(
-      `https://api.nasa.gov/planetary/apod?date=${date}&api_key=JZLWuJDR9crbBSjYEFfoziVpdkNQq6FNywPhfzdT`
-    );
-
-    const json = await response.json();
-    console.log(json);
-
-    if (json.code && json.msg) {
+    let response;
+    let json;
+    try {
+      response = await fetch(
+        `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${env.API_KEY}`
+      )
+      console.log("response: ", response)  
+      json = await response.json();
+      console.log("json: ", json);
+    } catch (error) {
       setTitle("");
       setDescription("");
-      setContent(<p>{json.msg}</p>);
+      setNextContent(<p>Valami hiba történt</p>);
+
+      return Promise.reject(response)
+    }
+
+    if (json.code && json.msg || json.error && json.error.message) {
+      setTitle("");
+      setDescription("");
+      setNextContent(<p>{json.msg || json.error.message}</p>);
     } else {
       setTitle(json.title);
       setDescription(json.explanation);
 
       if (json.media_type && json.media_type === "video")
-        setContent(
+        setNextContent(
           <iframe
             title={json.title}
             src={`${json.url}?autoplay=1&mute=1`}
           ></iframe>
         );
-      else setContent(<img alt="" src={`${json.url}`} />);
+      else setNextContent(<img alt="" src={`${json.url}`} />);
     }
   };
 
-  const prev = (e) => {
-    if (imageWithDescriptionClass.includes("hide"))
-      return;
+  const checkDate = (value) => {
+    const dateToSet = new Date(value);
+    const currentDate = new Date(date);
+    setShiftLeft(dateToSet < currentDate ? true: false);
 
-    setDate((prevDate) =>
-      new Date(prevDate).toString() === "Invalid Date"
-        ? new Date().toISOString().slice(0, 10)
-        : new Date(new Date(prevDate).getTime() - 1 * 24 * 60 * 60 * 1000)
-            .toISOString()
-            .slice(0, 10)
-    );
-    animate(true);
-    callingTheAPI();
-  };
+    return dateToSet.toString() === "Invalid Date"
+    ? new Date().toISOString().slice(0, 10)
+    : dateToSet.toISOString().slice(0, 10)
+  }
 
-  const next = (e) => {
-    if (imageWithDescriptionClass.includes("hide"))
-      return;
-
-    setDate((prevDate) =>
-      new Date(prevDate).toString() === "Invalid Date"
-        ? new Date().toISOString().slice(0, 10)
-        : new Date(new Date(prevDate).getTime() + 1 * 24 * 60 * 60 * 1000)
-            .toISOString()
-            .slice(0, 10)
-    );
-    animate(true);
-    callingTheAPI();
-  };
-
-  const onChange = (e) => {
-    setDate(
-      new Date(e.target.value).toString() === "Invalid Date"
-        ? new Date().toISOString().slice(0, 10)
-        : e.target.value
-    );
-  };
+  const increaseDecreaseDate = (increase = false) => {
+    const lengthOfOneDayInMiliseconds = 24 * 60 * 60 * 1000;
+    const value = increase ? lengthOfOneDayInMiliseconds : (-1) * lengthOfOneDayInMiliseconds;
+    setDate((prevDate) => {
+      const prevDateTimeStamp = new Date(prevDate).getTime();
+      const nextDateTimeStamp = prevDateTimeStamp + value;
+      return checkDate(nextDateTimeStamp);
+    });
+  }
 
   useEffect(() => {
-    animate(false);
-  }, [content]);
+    if (isContentOut && nextContent) {
+      setCurrentContent(nextContent);
+      setNextContent(null);
+      setShowContent(true);
+    } 
+  }, [isContentOut, nextContent]);
 
   useEffect(() => {
-    animate(true);
-    callingTheAPI();
-  }, []);
+    callAPI();
+  }, [date]);
 
   return (
     <div
-      className="Home"
+      className={styles.Home}
       style={{
         backgroundImage: 'url("bkg.jpg")',
         backgroundSize: "cover",
       }}
-      ref={componentRef}
     >
-      <div className="dashboard">
+      <div className={styles.dashboard}>
         <button
-          disabled={imageWithDescriptionClass.includes("hide")}
-          onClick={prev}
+          onClick={() => increaseDecreaseDate()}
         >
           -
         </button>
         <div>
           <input
-            disabled={imageWithDescriptionClass.includes("hide")}
             type="date"
             value={date}
-            onChange={onChange}
+            onChange={(e) => setDate(checkDate(e.target.value))}
           />
           <button
-            disabled={imageWithDescriptionClass.includes("hide")}
-            onClick={(e) => {
-              animate(true);
-              callingTheAPI();
-            }}
+            onClick={() => callAPI()}
           >
             Set
           </button>
         </div>
         <button
-          disabled={imageWithDescriptionClass.includes("hide")}
-          onClick={next}
+          onClick={() => increaseDecreaseDate(true)}
         >
           +
         </button>
       </div>
-      <div className="content">
-        <span onClick={prev}>{"\u21e6"}</span>
-        <div className={imageWithDescriptionClass} ref={contentRef}>
-          {title && (
-            <h2
-              onClick={(e) =>
-                descriptionClass.includes("show")
-                  ? setDescriptionClass("description")
-                  : setDescriptionClass("description show")
-              }
-            >
-              {title}
-            </h2>
-          )}
-          <div className="image">
-            {content}
-            {description && <div className={descriptionClass}>{description}</div>}
+      <div className={styles.content}>
+        <span onClick={() => increaseDecreaseDate()}>{"\u21e6"}</span>
+        <CSSTransition in={showContent} timeout={500} nodeRef={contentRef} classNames={{
+          enter: `animate__animated animate__bounce${shiftLeft ? 'InRight' : 'InLeft'}`,
+          enterDone: `animate__animated animate__bounce${shiftLeft ? 'InRight' : 'InLeft'}`,
+          exit: `animate__animated animate__bounce${shiftLeft ? 'OutLeft' : 'OutRight'}`,
+        }} onExited={() => setIsContentOut(true)} onEntered={() => setIsContentOut(false)}>
+          <div className={styles.imageWithDescription} ref={contentRef}>
+            {title && (
+              <h2
+                onClick={() =>
+                  setShowDescription(prev => !prev)
+                }
+              >
+                {title}
+              </h2>
+            )}
+            <div className={styles.image}>
+              {currentContent}
+              {description && <div className={classnames(styles.description, {[styles.show]: showDescription})}>{description}</div>}
+            </div>
           </div>
-        </div>
-        <span onClick={next}>{"\u21e8"}</span>
+        </CSSTransition>
+        <span onClick={() => increaseDecreaseDate(true)}>{"\u21e8"}</span>
       </div>
     </div>
   );
