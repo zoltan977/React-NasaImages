43b9067e1bbaa0c645d1f0c66f279dac06aa94d6
zoltanmeszaros977 <zoltanmeszaros977@gmail.com>
2023-02-09 01:20:33 +0100

    Adding image positioning feature to content compoent

--- src/components/shared/content/content.component.jsx
+++ src/components/shared/content/content.component.jsx
@@ -1,8 +1,22 @@
 import styles from "./content.module.scss";
-import React from "react";
+import React, { useState } from "react";
 import classnames from "classnames";
 
 export default function Content({ data, isClickable }) {
+  const [movement, setMovement] = useState(null);
+  const [start, setStart] = useState(null);
+
+  const onMouseDown = (e) => {
+    e.preventDefault();
+    setStart({ x: e.clientX, y: e.clientY });
+  };
+  const onMouseMove = (e) => {
+    setMovement({ x: e.clientX, y: e.clientY });
+  };
+  const onMouseUp = (e) => {
+    setStart(null);
+  };
+
   return (
     data &&
     data?.title &&
@@ -15,7 +29,21 @@ export default function Content({ data, isClickable }) {
             src={`${data.url}?autoplay=1&mute=1`}
           ></iframe>
         ) : (
-          <img alt="" src={`${data.url}`} />
+          <img
+            alt=""
+            src={`${data.url}`}
+            style={
+              start &&
+              movement && {
+                objectPosition: `calc(50% + ${
+                  movement.x - start.x
+                }px) calc(50% + ${movement.y - start.y}px)`,
+              }
+            }
+            onMouseDown={onMouseDown}
+            onMouseMove={onMouseMove}
+            onMouseUp={onMouseUp}
+          />
         )}
       </div>
     )
